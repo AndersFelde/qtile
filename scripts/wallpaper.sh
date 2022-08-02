ae5a1ed3da178fc372522e0d656b8d4a46d2959c
AndersFelde <anderskfelde@hotmail.com>
2022-08-02 09:25:39 +0200

    slippe å lage ny wallpaper hver oppstart

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 wallpaper="/usr/share/endeavouros/backgrounds/eos_wallpapers_community/Endy_planet.png"
 feh --bg-scale $wallpaper
-betterlockscreen -u $wallpaper & disown
+
+if [ -n "$(ls -A /home/kippster/.cache/betterlockscreen/current/ 2>/dev/null)" ]
+then
+    #contains file
+    echo "Already created wallpaper"
+else
+    #does not contain file
+    betterlockscreen -u $wallpaper & disown
+fi
