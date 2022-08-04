7c56d88c820f7337b396665da98595a54ccbdb7f
AndersFelde <anderskfelde@hotmail.com>
2022-08-04 11:48:10 +0200

    locker til 10 min

--- scripts/autolock.sh
+++ scripts/autolock.sh
@@ -6,7 +6,7 @@ locker="betterlockscreen -l blur"
 killer="systemctl suspend"
 notifier='notify-send "Locking in 10 sec" "betterlockscreen -l blur" -a locker -u critical'
 
-xautolock -time 5 -locker "$locker" -nowlocker \
+xautolock -time 10 -locker "$locker" -nowlocker \
 "$locker" -killtime 10 -killer "$killer" -notify 10 \
 -notifier "$notifier" -detectsleep -noclose
 
