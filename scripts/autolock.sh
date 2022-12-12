00e2c8961986eaea8696161ed092d7301b58ec5b
AndersFelde <anderskfelde@hotmail.com>
2022-12-12 12:55:37 +0100

    autolock

--- scripts/autolock.sh
+++ scripts/autolock.sh
@@ -4,7 +4,7 @@ xset s 900 #15 min
 
 locker="betterlockscreen -l blur"
 killer="systemctl suspend"
-notifier='notify-send "Locking in 10 sec" "betterlockscreen -l blur" -a locker -u critical'
+notifier='notify-send "Locking in 10 sec" "betterlockscreen -l blur" -a locker -u normal'
 
 xautolock -time 10 -locker "$locker" -nowlocker \
 "$locker" -killtime 10 -killer "$killer" -notify 10 \
