4e4f2d6691015c1892552f530d76304c54b8d4a4
AndersFelde <anderskfelde@hotmail.com>
2023-05-27 13:07:55 +0200

    2 monitors

--- modules/screens.py
+++ modules/screens.py
@@ -1,13 +1,22 @@
 from libqtile import bar
-from .widgets import barWidgets
 from libqtile.config import Screen
-from . import defaults
 
+from . import defaults
+from .widgets import barWidgets
 
 screens = [
     Screen(
         top=bar.Bar(
             barWidgets,
+            # leftWidgets + middleWidgets,
+            size=30,
+            margin=10,
+            background=defaults.colors["transparent"],
+        ),
+    ),
+    Screen(
+        top=bar.Bar(
+            barWidgets.copy(),
             # leftWidgets + middleWidgets,
             size=30,
             margin=10,
