a5148fd7bad483c48ef40019d0fe14842f41a9f7
AndersFelde <anderskfelde@hotmail.com>
2024-08-10 12:09:37 +0200

    ok

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,9 @@
 ~/.config/qtile/scripts/autolock.sh & disown
 ~/.config/qtile/scripts/wallpaper.sh & disown
 
+#global settings
+xsettingsd & disown
+
 picom & disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
 
 # Low battery notifier
